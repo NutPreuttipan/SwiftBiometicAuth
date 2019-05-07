582efe6a8a80fa2598c50ad1a5fcbc7d71f3a7f7
Preuttipan <terrorize.nut@gmail.com>
2019-05-07 11:49:36 +0700

    Initial Commit

---
+++ SwiftBiometicAuth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBiometicAuth
+//
+//  Created by Preuttipan Janpen on 7/5/2562 BE.
+//  Copyright © 2562 Lphant Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
